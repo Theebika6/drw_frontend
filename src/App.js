233663a837946e0efe981ea32a05909ce3eb8558
Patrickriachi <patrickriachi123@gmail.com>
2024-01-20 19:47:05 -0500

    Sidebar Collapse and Extend options

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Sidebar from './Components/Sidebar/Sidebar';
-import Header from './Components/Header/Header';
 import AboutUs from './Components/About-Us/AboutUs';
 
 const App = () => {
@@ -9,7 +8,6 @@ const App = () => {
         <Router>
             <div className="App">
                 <Sidebar />
-                <Header />
                 <Routes>
                     <Route path="/" element={<AboutUs />} />
                     {/* Add more routes here as needed */}
--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -2,15 +2,24 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Sidebar.css';
 import drwLogo from '../Images/Logos/drw_logo_white.png';
+import collapsedLogo from '../Images/Logos/DRW_logo.png';
 import aboutUsIcon from '../Images/Sidebar/about-us.png';
 import mapIcon from '../Images/Sidebar/map.png';
-import analyticsIcon from '../Images/Sidebar/analytics.png'
+import analyticsIcon from '../Images/Sidebar/analytics.png';
+import leftArrow from '../Images/Sidebar/left-arrow.png';
+import rightArrow from '../Images/Sidebar/right-arrow.png';
+
 
 const Sidebar = () => {
     const [activeLink, setActiveLink] = useState(null);
+    const [isCollapsed, setIsCollapsed] = useState(false);
 
     const handleLinkClick = (linkName) => {
         setActiveLink(linkName);
+    };
+
+    const toggleSidebar = () => {
+        setIsCollapsed(!isCollapsed);
     };
 
     const sidebarItems = [
@@ -32,8 +41,8 @@ const Sidebar = () => {
     ];
 
     return (
-        <div className="sidebar">
-            <img src={drwLogo} alt="DRW Logo" className="sidebar-logo"/>
+        <div className={`sidebar ${isCollapsed ? 'collapsed' : ''}`}>
+            <img src={isCollapsed ? collapsedLogo : drwLogo} alt="Logo" className="sidebar-logo"/>
             <ul>
                 {sidebarItems.map((item, index) => (
                     <li key={index}>
@@ -43,11 +52,14 @@ const Sidebar = () => {
                             onClick={() => handleLinkClick(item.name)}
                         >
                             <img src={item.icon} alt={`${item.name} Icon`} className="sidebar-icon"/>
-                            <span>{item.name}</span>
+                            {!isCollapsed && <span>{item.name}</span>}
                         </Link>
                     </li>
                 ))}
             </ul>
+            <div className="sidebar-toggle" onClick={toggleSidebar}>
+                <img src={isCollapsed ? rightArrow : leftArrow} alt="Toggle Sidebar" className="toggle-icon"/>
+            </div>
         </div>
     );
 };
